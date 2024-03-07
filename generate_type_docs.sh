b898c9b6351e9cb5f4cb60639aafb439ffbc457b
Alice Pote <alice.writes.wrongs@gmail.com>
2024-03-07 16:06:28 -0500

    ok

--- generate_type_docs.sh
+++ generate_type_docs.sh
@@ -12,4 +12,15 @@ npx typedoc \
   src/declarations/stencil-public-runtime.ts \
   src/declarations/stencil-public-compiler.ts
 
+# markdown mode
+npx typedoc \
+  --plugin typedoc-plugin-markdown \
+  --excludeReferences \
+  src/cli/index.ts \
+  src/compiler/public.ts \
+  src/sys/node/public.ts \
+  src/declarations/stencil-public-runtime.ts \
+  src/declarations/stencil-public-compiler.ts \
+  --out ../stencil-site/api
+
 bippy --dir docs
