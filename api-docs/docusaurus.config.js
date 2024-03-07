4be9187eaec948f376729b5a27d59e9ae4d180d8
Alice Pote <alice.writes.wrongs@gmail.com>
2024-03-07 16:05:14 -0500

    fairly complete setup for docusaurus typedoc site

---
+++ api-docs/docusaurus.config.js
@@ -0,0 +1,51 @@
+const HOSTNAME = 'stenciljs.com';
+
+module.exports = {
+  title: "Stencil API Docs",
+  url: `https://${HOSTNAME}`,
+  baseUrl: '/',
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
+        }
+      }
+    ]
+  ],
+  markdown: {
+    format: 'md',
+  },
+  // presets: [
+  //   [
+  //     '@ionic-docs/preset-classic',
+  //     {}
+  //   ]
+  // ],
+  plugins: [
+    [
+      'docusaurus-plugin-typedoc',
+
+      // Plugin / TypeDoc options
+      {
+        entryPoints: [
+          '../src/cli/index.ts',
+          '../src/compiler/public.ts',
+          '../src/sys/node/public.ts',
+          '../src/declarations/stencil-public-runtime.ts',
+          '../src/declarations/stencil-public-compiler.ts',
+        ],
+        excludeReferences: true,
+        tsconfig: '../tsconfig.json',
+        out: ".",
+        // sidebar: {
+        //   // categoryLabel: 'API XYZ',
+        //   collapsed: false,
+        //   position: 0,
+        //   fullNames: true,
+        // },
+      },
+    ],
+  ],
+};
