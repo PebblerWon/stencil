3968590e4ba580dd154386cfb8af697a62675e15
Alice Pote <alice.writes.wrongs@gmail.com>
2024-03-07 19:22:55 -0500

    likewise

--- src/runtime/vdom/vdom-render.ts
+++ src/runtime/vdom/vdom-render.ts
@@ -384,7 +384,7 @@ const removeVnodes = (vnodes: d.VNode[], startIdx: number, endIdx: number) => {
  * @param newCh the new children of the parent node
  * @param isInitialRender whether or not this is the first render of the vdom
  */
-export const updateChildren = (
+const updateChildren = (
   parentElm: d.RenderNode,
   oldCh: d.VNode[],
   newVNode: d.VNode,
